5cdbcb74ae4951b1ee20999f1ecaea80f9623249
Chris <70657828+BabalauCristian10C@users.noreply.github.com>
2022-09-15 21:18:38 +0500

    Update 3pr.py

--- 3pr.py
+++ 3pr.py
@@ -1,6 +1,6 @@
 a,b,c,d = int(input()), int(input()),int(input()), int(input())
 
 def fract(a,b,c,d):
-    return [((a*c)/(b*d)), ((a*d)/(b*c))]
+    return [((a*d+c*b)/(b*d))]
 
 print(fract(a,b,c,d))
